0dc3b521d49830e715c3a7f94965bd8883975376
Joe Herbert <joe@joeherbert.dev>
2024-02-18 18:54:11 +0000

    Commit searchdown to git

---
+++ example.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const valueList = ["France", "England", "Spain", "Wales", "Ireland", "Germany", "Portugal", "Italy"];
+    searchdown("basic", {
+        values: valueList,
+        sort: undefined, //undefined, "ASC", "DESC"
+        limit: 0, //0 means no limit and is default
+        multiple: false,
+        addValues: false,
+        caseSensitive: true,
+        placeholder: "Search",
+        inputName: "basic",
+    });
+    searchdown("multiple", {
+        values: ["France", "England", "Spain", "Wales", "Ireland", "Germany", "Portugal", "Italy"],
+        sort: "ASC", //undefined, "ASC", "DESC"
+        limit: 0, //0 means no limit and is default
+        multiple: true,
+        addValues: false,
+        caseSensitive: true,
+        placeholder: "Enter a country", //default is "Search"
+        inputName: "multiple",
+    });
+    searchdown("addValue", {
+        values: ["France", "England", "Spain", "Wales", "Ireland", "Germany", "Portugal", "Italy"],
+        sort: "DESC", //undefined, "ASC", "DESC"
+        limit: 0, //0 means no limit and is default
+        multiple: false,
+        addValues: true,
+        caseSensitive: false,
+        placeholder: "Search",
+        inputName: "addValue",
+    });
+    searchdown("multiple-addValue", {
+        values: valueList,
+        sort: undefined, //undefined, "ASC", "DESC"
+        limit: 0, //0 means no limit and is default
+        multiple: true,
+        addValues: true,
+        caseSensitive: true,
+        placeholder: "Search",
+        saveEntered: false,
+        inputName: "multipleAddValue",
+    });
+});
